a25ce397a5fe4ae4890e0023cdf6debbf0d348e3
Kasper Ahlroth <kasper.ahlroth@gmail.com>
2021-12-23 17:14:13 +0200

    Commit for publishing

--- src/Add.js
+++ src/Add.js
@@ -7,13 +7,20 @@ import { Camera } from 'expo-camera';
 import { Ionicons } from '@expo/vector-icons';
 import { push, ref, getDatabase } from 'firebase/database';
 import styles from './components/StyleComponent';
-import FirebaseConfig from './components/FirebaseConfig';
 
 import { initializeApp } from "firebase/app";
 
 export default function Add() {
 
-  const firebaseConfig = {FirebaseConfig};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDeDMA8a4xmgiED51iipi9BJm1dM-IK4aE",
+    authDomain: "watchdb-a0222.firebaseapp.com",
+    databaseURL: "https://watchdb-a0222-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "watchdb-a0222",
+    storageBucket: "watchdb-a0222.appspot.com",
+    messagingSenderId: "782456172187",
+    appId: "1:782456172187:web:0b7eb98bae565330d7e03e"
+  };
     
   const app = initializeApp(firebaseConfig);
   const database = getDatabase(app);
--- src/WatchView.js
+++ src/WatchView.js
@@ -3,13 +3,20 @@ import { View, StatusBar, FlatList, Image } from 'react-native';
 import { ListItem, Button } from 'react-native-elements';
 import { getDatabase, ref, onValue, remove }  from"firebase/database";
 import styles from './components/StyleComponent';
-import FirebaseConfig from './components/FirebaseConfig';
 
 import { initializeApp } from "firebase/app";
 
 export default function WatchView() {
 
-    const firebaseConfig = {FirebaseConfig};
+    const firebaseConfig = {
+        apiKey: "AIzaSyDeDMA8a4xmgiED51iipi9BJm1dM-IK4aE",
+        authDomain: "watchdb-a0222.firebaseapp.com",
+        databaseURL: "https://watchdb-a0222-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "watchdb-a0222",
+        storageBucket: "watchdb-a0222.appspot.com",
+        messagingSenderId: "782456172187",
+        appId: "1:782456172187:web:0b7eb98bae565330d7e03e"
+    };
     
     const app = initializeApp(firebaseConfig);
     const database = getDatabase(app);
